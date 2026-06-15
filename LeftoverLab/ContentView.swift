daf36e6bafd87f46743eda71e4b11dde7edc9009
Jay Parmar <jhb9204@gmail.com>
2026-06-14 18:58:21 -0700

    Initial Commit

---
+++ LeftoverLab/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LeftoverLab
+//
+//  Created by Jay Parmar on 14/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LeftoverLab/LeftoverLabApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeftoverLabApp.swift
+//  LeftoverLab
+//
+//  Created by Jay Parmar on 14/06/26.
+//
+
+import SwiftUI
+
+@main
+struct LeftoverLabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
